26b4602a199c6b5885abe70846db0f7419fa36ff
ot-nemoto <nemoto@opentone.co.jp>
2019-02-28 10:10:35 +0000

    SAMLアサーションで直接AWSを表示させるボタンを追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
   end
+  def aws_openam
+    render html: Idp::Openam.new(current_user).aws_saml_assertion.body.html_safe
+  end
 end
--- lib/idp/openam.rb
+++ lib/idp/openam.rb
@@ -25,6 +25,17 @@ class Idp::Openam
     JSON.parse(res.body, { symbolize_names: true })[:tokenId]
   end
 
+  def token_id
+    uri = URI.parse("#{ENV["OPENAM_URI"] || DEFAULT_URL}/json/authenticate?realm=/")
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Post.new(uri.request_uri)
+    req["X-OpenAM-Username"] = email
+    req["X-OpenAM-Password"] = password
+    req["Content-Type"] = "application/json"
+    res = http.request(req)
+    JSON.parse(res.body, { symbolize_names: true })[:tokenId]
+  end
+
   def create_user
     uri = URI.parse("#{ENV["OPENAM_URI"] || DEFAULT_URL}/json/users/?_action=create")
     http = Net::HTTP.new(uri.host, uri.port)
@@ -61,6 +72,14 @@ class Idp::Openam
     req = Net::HTTP::Delete.new(uri.request_uri)
     req["iplanetDirectoryPro"] = admin_token_id
     req["Content-Type"] = "application/json"
+    http.request(req)
+  end
+
+  def aws_saml_assertion
+    uri = URI.parse("#{ENV["OPENAM_URI"] || DEFAULT_URL}/saml2/jsp/idpSSOInit.jsp?metaAlias=/idp&spEntityID=urn:amazon:webservices")
+    http = Net::HTTP.new(uri.host, uri.port)
+    req = Net::HTTP::Get.new(uri.request_uri)
+    req["Cookie"] = "iPlanetDirectoryPro=#{token_id}"
     http.request(req)
   end
 end
